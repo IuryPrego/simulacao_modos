4abc5bcdcd775412b079c1b0e22047c85028f661
IuryPrego <iurypregoqw@gmail.com>
2025-12-15 23:09:06 -0300

    bug correction

--- core/metrics.py
+++ core/metrics.py
@@ -14,5 +14,8 @@ def phase(E):
 def power(x,y,E):
     dx = float(x[0, 1] - x[0, 0])
     dy = float(y[1, 0] - y[0, 0])
-    power = np.sum(np.abs(E))** 2 * dx * dy
+    if E.ndim == 3:
+        E = np.linalg.norm(E,axis=2)
+
+    power = np.sum(np.abs(E)**2) * dx * dy
     return power
