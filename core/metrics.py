fff9ae4b2f3198de14c525cdf397d34463f785d8
IuryPrego <iurypregoqw@gmail.com>
2025-12-15 22:35:12 -0300

    bug correction

--- core/metrics.py
+++ core/metrics.py
@@ -1,9 +1,12 @@
+from matplotlib import pyplot as plt
 import numpy as np
 
-def intensity(E):
+def intensity(E,cmap='viridis'):
     if E.ndim == 3:
         E = np.linalg.norm(E,axis=2)
-    return np.abs(E)**2
+    E = np.abs(E)**2
+    plt.imshow(E,cmap, vmin=0, vmax=max(1e-5,np.max(E)))
+
 
 def phase(E):
     return np.angle(E)
