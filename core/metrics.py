19ba03d7f458ea5caa0a81ca8a2f373291cb7e77
IuryPrego <iurypregoqw@gmail.com>
2025-12-18 12:23:35 -0300

    visualização de polarização

--- core/metrics.py
+++ core/metrics.py
@@ -1,15 +1,42 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-def intensity(field,cmap='viridis'):
+def intensity(field,cmap='viridis',t=0,pace=1,scale=None):
+    field_p = field[...]
     if field.ndim == 3:
         field = np.linalg.norm(field,axis=2)
     field = np.abs(field)**2
 
     plt.figure().frameon = False
+    plt.axis('equal')
     plt.axis('off')
     plt.imshow(field,cmap, vmin=0, vmax=max(1e-5,np.max(field)))
 
+    if field_p.ndim == 3:
+        x,y = np.meshgrid(np.linspace(0,len(field[0,:]),len(field[0,:])),np.linspace(0,len(field[0,:]),len(field[:,0])))
+        Ex = field_p[...,0]
+        Ey = field_p[...,1]
+
+        mod = np.sqrt(np.abs(Ex)**2 + np.abs(Ey)**2)
+        mod[mod == 0] = 1
+
+        Ex = Ex / mod * np.exp(-1j*t)
+        Ey = Ey / mod * np.exp(-1j*t)
+
+        u = np.real(Ex)
+        v = np.real(Ey)
+
+        u[:,0] = 0
+        v[:,0] = 0
+        u[0,:] = 0
+        v[0,:] = 0
+
+        plt.quiver(x[::pace, ::pace], y[::pace, ::pace],
+                u[::pace, ::pace],v[::pace, ::pace],
+                cmap='gray',
+                pivot='middle',
+                scale=scale)
+    
 
 def phase(field):
     return np.angle(field)
@@ -22,17 +49,3 @@ def power(x,y,field):
 
     power = np.sum(np.abs(field)**2) * dx * dy
     return power
-
-
-def polarization(x,y,field, t=0, pace=20,scale=None):
-    Ex = np.real(field[...,0] * np.exp(-1j*t))
-    Ey = np.real(field[...,1] * np.exp(-1j*t))
-
-    plt.figure()
-    plt.quiver(x[::pace, ::pace], y[::pace, ::pace],
-               Ex[::pace, ::pace],Ey[::pace, ::pace],
-               scale=scale)
-    
-    plt.axis('equal')
-    plt.axis('off')
-    plt.show()
