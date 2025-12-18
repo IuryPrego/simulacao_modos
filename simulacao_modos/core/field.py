690594450baafcab386f4908413a4d70eff9536e
IuryPrego <111467865+IuryPrego@users.noreply.github.com>
2025-12-18 10:49:48 -0300

    mudanças lab

---
+++ simulacao_modos/core/field.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+class OpticalField:
+    def __init__(self, x, y, z=0, wavelength=632.8e-9, E=None):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.dx = float(x[0, 1] - x[0, 0])
+        self.dy = float(y[1, 0] - y[0, 0])
+        self._lambda_ = wavelength
+        self.k = 2 * np.pi / wavelength
+        if np.sum(E) != None:
+            self.E = E
+        else:
+            self.E = np.zeros_like(x,dtype='complex128')
+
+
+
+    def intensity(self):
+        return np.abs(self.E)**2
+    
+    def Power(self):
+        dx, dy = self.dx,self.dy
+        power = np.sum(np.abs(self.E))** 2 * dx * dy
+        return power
+
+    def phase(self):
+        return np.angle(self.E)
+
+
+    def normalize(self):
+        norm = self.Power()
+        if norm != 0:
+            self.E /= np.sqrt(norm)
+        else:
+            raise ValueError('Can`t normalize empty field')
+
---
+++ simulacao_modos/core/metrics.py
@@ -0,0 +1,23 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def intensity(E,cmap='viridis'):
+    if E.ndim == 3:
+        E = np.linalg.norm(E,axis=2)
+    E = np.abs(E)**2
+    plt.imshow(E,cmap, vmin=0, vmax=max(1e-5,np.max(E)))
+    plt.tight_layout()
+    plt.axis('off')
+
+
+def phase(E):
+    return np.angle(E)
+
+def power(x,y,E):
+    dx = float(x[0, 1] - x[0, 0])
+    dy = float(y[1, 0] - y[0, 0])
+    if E.ndim == 3:
+        E = np.linalg.norm(E,axis=2)
+
+    power = np.sum(np.abs(E)**2) * dx * dy
+    return power
