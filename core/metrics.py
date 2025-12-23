bb1f246b9789061fddd4d956ffa10cc0651b97ac
IuryPrego <iurypregoqw@gmail.com>
2025-12-23 01:02:07 -0300

    metrics corrected

--- core/metrics.py
+++ core/metrics.py
@@ -1,7 +1,7 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-def intensity(field,cmap='viridis',t=0,pace=1,scale=None):
+def intensity(field,cmap='viridis',pace=50,scale=30,animate=False,t=0):
     field_p = field[...]
     if field.ndim == 3:
         field = np.linalg.norm(field,axis=2)
@@ -14,17 +14,30 @@ def intensity(field,cmap='viridis',t=0,pace=1,scale=None):
 
     if field_p.ndim == 3:
         x,y = np.meshgrid(np.linspace(0,len(field[0,:]),len(field[0,:])),np.linspace(0,len(field[0,:]),len(field[:,0])))
+
+        field_p[field/field.max() <= 1e-2] = 0
+
         Ex = field_p[...,0]
         Ey = field_p[...,1]
 
-        mod = np.sqrt(np.abs(Ex)**2 + np.abs(Ey)**2)
+        mod = np.sqrt(field)
         mod[mod == 0] = 1
 
-        Ex = Ex / mod * np.exp(-1j*t)
-        Ey = Ey / mod * np.exp(-1j*t)
+        if animate:
+            timephase = np.exp(-1j*t)
 
-        u = np.real(Ex)
-        v = np.real(Ey)
+            Ex = Ex / mod * timephase
+            Ey = Ey / mod * timephase
+
+            u = np.real(Ex)
+            v = np.real(Ey)
+        else:
+            Ex = Ex / mod
+            Ey = Ey / mod
+
+            u = np.abs(Ex)
+            v = np.abs(Ey)
+
 
         u[:,0] = 0
         v[:,0] = 0
