97f3d4546bcba1c1a007adff53aaf683f21d4f71
IuryPrego <iurypregoqw@gmail.com>
2026-05-19 10:43:06 -0300

    tá gambiarra, mas depois eu organizo

--- core/IntensityModes.py
+++ core/IntensityModes.py
@@ -1,6 +1,7 @@
 import numpy as np
-from scipy.special import genlaguerre, hermite
+from scipy.special import genlaguerre, hermite, eval_hermite, gammaln
 from core.metrics import power
+import math
 
 # creation of intensity modes laguerre and hermite gauss beams
 # the functions get x,y and beams parameters to create and return a escalar field
@@ -69,3 +70,57 @@ def hermite_gauss(x,y,m=0,n=0,z=0,w0=1e-3,thetax=0,thetay=0,wavelength=632.8e-9,
         return E/np.sqrt(norm)
     else:
         return E
+
+
+def hermite_gauss_b(x, y, m=0, n=0, z=0, w0=1e-3,
+                  thetax=0, thetay=0, wavelength=632.8e-9):
+
+    k = 2 * np.pi / wavelength
+    zR = np.pi * w0**2 / wavelength
+    w = w0 * np.sqrt(1 + (z / zR)**2)
+
+    # coordenadas reduzidas
+    x1 = x[0, :]
+    y1 = y[:, 0]
+
+    xi = np.sqrt(2) * x1 / w
+    yi = np.sqrt(2) * y1 / w
+
+    # normalização analítica
+    log_norm = (
+        - np.log(w)
+        + 0.5 * np.log(2/np.pi)
+        - 0.5 * ((m+n)*np.log(2) + gammaln(m+1) + gammaln(n+1))
+        )
+
+    # hermites 1D
+    Hn = eval_hermite(n, xi)
+    Hm = eval_hermite(m, yi)
+
+    gx = Hn * np.exp(-x1**2 / w**2)
+    gy = Hm * np.exp(-y1**2 / w**2)
+
+    amplitude = np.outer(gy, gx)*np.exp(log_norm)
+
+    # fase
+    gouy = (m + n + 1) * np.arctan(z / zR)
+
+    if z != 0:
+        R = z * (1 + (zR / z)**2)
+        phase = k*z + k*(x**2 + y**2)/(2*R) - gouy
+    else:
+        phase = k*z - gouy
+
+    tilt = np.exp(1j * k * (thetax * x + thetay * y))
+
+    return amplitude * np.exp(1j * phase) * tilt
+
+def hg_1d_n(x, n, w):
+    from scipy.special import eval_hermite, gammaln
+    
+    xi = np.sqrt(2) * x / w
+    
+    log_norm = -0.5 * (n*np.log(2) + gammaln(n+1) + 0.5*np.log(np.pi))
+    norm = np.exp(log_norm)
+    
+    return norm * eval_hermite(n, xi) * np.exp(-xi**2 / 2)
