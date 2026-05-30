e4b60bc5ee9a3674fee854e1cb4f8d6b218a4866
IuryPrego <iurypregoqw@gmail.com>
2026-05-30 01:22:15 -0300

    hermite definitive version and 1d version

--- core/IntensityModes.py
+++ core/IntensityModes.py
@@ -1,79 +1,35 @@
+from math import factorial
+
 import numpy as np
 from scipy.special import genlaguerre, hermite, eval_hermite, gammaln
-from core.metrics import power
-import math
 
 # creation of intensity modes laguerre and hermite gauss beams
 # the functions get x,y and beams parameters to create and return a escalar field
 
-def laguerre_gauss(x,y,l=0,p=0,z=0,w0=1e-3,theta_x=0,theta_y=0,wavelength=632.8e-9,normalize=True):
-    dx = float(x[0, 1] - x[0, 0])
-    dy = float(y[1, 0] - y[0, 0])
+def laguerre_gauss(x,y,l=0,p=0,z=0,w0=1e-3,wavelength=632.8e-9):
     r = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
     k = 2 * np.pi / wavelength
     zR = np.pi * w0**2 / wavelength
     w = w0 * np.sqrt(1 + (z / zR)**2)
-    norm = 0
+    log_norm = (0.5*(np.log(2) + gammaln(p+1) - np.log(np.pi) - gammaln(p+abs(l)+1))
+                - np.log(w))
 
     L = genlaguerre(p, abs(l))(2*r**2/w**2)
 
-    amplitude = (w0 / w) * ((np.sqrt(2) * r / w)**abs(l)) * L * np.exp(-r**2 / w**2)
-    gouy_phase = (2*p + abs(l) + 1) * np.arctan(z / zR)
-    tilt = np.exp(1j*k*(theta_x)*x) * np.exp(1j*k*(theta_y)*y)
-    
+    amplitude = ((np.sqrt(2) * r / w)**abs(l)) * L * np.exp(-r**2 / w**2)
     if z != 0:
+        gouy_phase = (2*p + abs(l) + 1) * np.arctan(z / zR)
         R = z * (1 + (zR / z)**2)
         phase = k * z + k * r**2 / (2 * R) - gouy_phase + l * phi
     else:
-        phase = - gouy_phase + l * phi
+        phase = l * phi
 
-    E = amplitude * np.exp(1j * phase) * tilt
+    E = amplitude * np.exp(1j * phase)
 
-    if normalize:
-        norm = power(x,y,E)
+    return E*np.exp(log_norm)
 
-    if norm != 0:
-        return E/np.sqrt(norm)
-    else:
-        return E
-
-
-def hermite_gauss(x,y,m=0,n=0,z=0,w0=1e-3,thetax=0,thetay=0,wavelength=632.8e-9,normalize=True):
-    dx = float(x[0, 1] - x[0, 0])
-    dy = float(y[1, 0] - y[0, 0])
-    k = 2 * np.pi / wavelength
-    zR = np.pi * w0**2 / wavelength
-    w = w0 * np.sqrt(1 + (z / zR)**2)
-    norm = 0
-
-    H_n = hermite(n)(np.sqrt(2)*x/w)
-    H_m = hermite(m)(np.sqrt(2)*y/w)
-
-    amplitude = H_n*np.exp(-x**2/w**2)*H_m*np.exp(-y**2/w**2)
-    gouy_phase = (m + n + 1) * np.arctan(z / zR)
-    tilt = np.exp(1j*k*(thetax)*x) * np.exp(1j*k*(thetay)*y)
-    
-    if z != 0:
-        R = z * (1 + (zR / z)**2)
-        phase = k * z + k * (x**2+y**2) / (2 * R) - gouy_phase
-    else:
-        phase = k * z - gouy_phase
-
-
-    E = amplitude * np.exp(1j * phase) * tilt
-
-    if normalize:
-        norm = power(x,y,E)
-
-    if norm != 0:
-        return E/np.sqrt(norm)
-    else:
-        return E
-
-
-def hermite_gauss_b(x, y, m=0, n=0, z=0, w0=1e-3,
-                  thetax=0, thetay=0, wavelength=632.8e-9):
+def hermite_gauss(x, y, m=0, n=0, z=0, w0=1e-3, wavelength=632.8e-9):
 
     k = 2 * np.pi / wavelength
     zR = np.pi * w0**2 / wavelength
@@ -83,44 +39,38 @@ def hermite_gauss_b(x, y, m=0, n=0, z=0, w0=1e-3,
     x1 = x[0, :]
     y1 = y[:, 0]
 
-    xi = np.sqrt(2) * x1 / w
-    yi = np.sqrt(2) * y1 / w
+    a = np.sqrt(2) / w
 
     # normalização analítica
-    log_norm = (
-        - np.log(w)
-        + 0.5 * np.log(2/np.pi)
-        - 0.5 * ((m+n)*np.log(2) + gammaln(m+1) + gammaln(n+1))
-        )
+    log_norm = 0.5 * np.log(a)
+    log_norm = (- np.log(w)
+                + 0.5 * np.log(2/np.pi)
+                - 0.5 * ((m+n)*np.log(2) + gammaln(m+1) + gammaln(n+1)))
 
     # hermites 1D
-    Hn = eval_hermite(n, xi)
-    Hm = eval_hermite(m, yi)
+    Hn = eval_hermite(n, a*x1)
+    Hm = eval_hermite(m, a*y1)
 
-    gx = Hn * np.exp(-x1**2 / w**2)
-    gy = Hm * np.exp(-y1**2 / w**2)
+    gx = Hn * np.exp(-(a*x1)**2 / 2)
+    gy = Hm * np.exp(-(a*y1)**2 / 2)
 
     amplitude = np.outer(gy, gx)*np.exp(log_norm)
 
     # fase
-    gouy = (m + n + 1) * np.arctan(z / zR)
-
     if z != 0:
+        gouy = (m + n + 1) * np.arctan(z / zR)
         R = z * (1 + (zR / z)**2)
         phase = k*z + k*(x**2 + y**2)/(2*R) - gouy
     else:
-        phase = k*z - gouy
+        phase = 0
 
-    tilt = np.exp(1j * k * (thetax * x + thetay * y))
+    return amplitude * np.exp(1j * phase)
 
-    return amplitude * np.exp(1j * phase) * tilt
-
-def hg_1d_n(x, n, w):
-    from scipy.special import eval_hermite, gammaln
+def hg_1d_n(x, m, w=1e-3):
+    a = np.sqrt(2) / w
+    xi = a * x 
     
-    xi = np.sqrt(2) * x / w
-    
-    log_norm = -0.5 * (n*np.log(2) + gammaln(n+1) + 0.5*np.log(np.pi))
+    log_norm = 0.5*np.log(a) - 0.5 * (m*np.log(2) + gammaln(m+1) + 0.5*np.log(np.pi))
     norm = np.exp(log_norm)
     
-    return norm * eval_hermite(n, xi) * np.exp(-xi**2 / 2)
+    return norm * eval_hermite(m, xi) * np.exp(-xi**2 / 2)
